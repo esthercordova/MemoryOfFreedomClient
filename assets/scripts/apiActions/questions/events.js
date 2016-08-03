d40f4c50a012af66c054bac299b575b3a6b09426
esthercordova <estherpiacordova@gmail.com>
2016-08-03 14:40:12 -0400

    PATCH status work for easy and hard without a note
    - still hardcoded note

--- assets/scripts/apiActions/questions/events.js
+++ assets/scripts/apiActions/questions/events.js
@@ -14,7 +14,7 @@ const onPopulatingQuestions = () => {
   .fail(ui.failure);
 };
 
-const onSaveStatus = (event) => {
+const onSaveStatusEasy = (event) => {
   event.preventDefault();
   let user_id = app.user.id;
   let notes = "note";
@@ -28,6 +28,21 @@ const onSaveStatus = (event) => {
   .done(ui.success)
   .fail(ui.failure);
   };
+
+  const onSaveStatusHard = (event) => {
+    event.preventDefault();
+    let user_id = app.user.id;
+    let notes = "note";
+    let status = "hard";
+    let dataIdNextQuestion = $(".giveQuestionId").data("id");
+    let question_id = dataIdNextQuestion -1;
+    console.log("question_id ", question_id);
+
+    console.log( status + " userid " + user_id + "question_id "+ question_id);
+    api.changeQuestionStatus(user_id, question_id, status,  notes)
+    .done(ui.success)
+    .fail(ui.failure);
+    };
 
 const onShowStatictics = (event) => {
   event.preventDefault();
@@ -44,7 +59,8 @@ const onChooseWhatToStudy = () => {
 const addHandlers = () => {
   $(document).on('click','#start', onShowStatictics);
   $(document).on( 'click', '#start',onPopulatingQuestions);
-  $(document).on('click', '#right', onSaveStatus);
+  $(document).on('click', '#right', onSaveStatusEasy);
+  $(document).on('click', '#wrong', onSaveStatusHard);
   $(document).on('click', '#stop', onChooseWhatToStudy);
 };
 
@@ -52,6 +68,6 @@ module.exports = {
   addHandlers,
   onPopulatingQuestions,
   onShowStatictics,
-  onSaveStatus,
-
+  onSaveStatusEasy,
+  onSaveStatusHard,
 };
