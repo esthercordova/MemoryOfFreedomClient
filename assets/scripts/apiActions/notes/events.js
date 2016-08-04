1bf1ed10764be772e1b61113bfce45b8124c409f
esthercordova <estherpiacordova@gmail.com>
2016-08-04 11:30:02 -0400

    Set folder structure for notes

---
+++ assets/scripts/apiActions/notes/events.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// const getFormFields = require('../../../../lib/get-form-fields');
+const api = require('./api');
+const ui = require('./ui');
+const app = require('../../app');
+
+const showChooseWhatToStudyTemplate = require('../../../templates/chooseWhatToStudy.handlebars');
+
+
+const addHandlers = () => {
+
+};
+
+module.exports = {
+
+};
---
+++ assets/scripts/apiActions/notes/ui.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const app = require('../../app.js');
+const api = require('./api.js');
+const questionsEvents = require('./events.js');
+
+const showQuestionTemplate = require('../../../templates/showquestion.handlebars');
+const showStatisticTemplate = require('../../../templates/statistic.handlebars');
+
+
+module.exports = {
+
+};
